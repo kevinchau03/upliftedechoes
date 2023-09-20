6b13d4de21c192ab4588332a3a9a78881ed04be9
kevin <kckevinchau11@gmail.com>
2023-09-20 02:35:52 -0400

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import SearchBar from './components/searchBar';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Uplifted Echoes</h1>
+      <h2>Your Daily Dose of Serotonin</h2>
+      <SearchBar />
+    </div>
+  );
+}
+
+export default App;
