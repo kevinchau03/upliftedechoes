465e852bf9cbf4b4349fc473c12a60f183c68679
kevin <kckevinchau11@gmail.com>
2023-09-20 02:46:57 -0400

    changes

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <h1>Uplifted Echoes</h1>
-      <h2>Your Daily Dose of Serotonin</h2>
+      <h2>Your Daily Dose of Serotonin!!!</h2>
       <SearchBar />
     </div>
   );
