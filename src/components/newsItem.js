943cac0414b7ec1c5172287b51b07e16806f68a9
kevin <kckevinchau11@gmail.com>
2023-09-21 00:45:51 -0400

    api

--- src/components/newsItem.js
+++ src/components/newsItem.js
@@ -1,11 +1,11 @@
 import React from 'react';
 import '../App.css';
 
-const NewsItem = () => {
+const NewsItem = (props) => {
     return (
         <div className="news">
-            <p className="news-title">Example News</p>
-            <p className="news-description">Kevin is awesome kevin is super awesome kevin lOL</p>
+            <p className="news-title">{props.title}</p>
+            <p className="news-description">{props.desc}</p>
         </div>
     );
 };
--- src/components/searchBar.js
+++ src/components/searchBar.js
@@ -1,10 +1,47 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../App.css';
 
+const apiKey = process.env.REACT_APP_API_KEY;
+console.log(apiKey);
+
 const SearchBar = () => {
+    const [searchTerm, setSearchTerm] = useState('');
+    const [searchResults, setSearchResults] = useState(null);
+
+    async function getNews() {
+        try {
+            const response = await fetch(`https://newsapi.org/v2/top-headlines?country=us&q=${searchTerm}&apiKey=${apiKey}`);
+            const data = await response.json();
+            setSearchResults(data);
+            console.log(data);
+        } catch (error) {
+            console.error('Error fetching news:', error);
+        }
+    }
+
+    const handleSearch = (e) => {
+        e.preventDefault();
+        getNews();
+    }
+
     return (
         <div className="search">
-            <input class="bar" type="text" placeholder="Search for a topic" />
+            <form onSubmit={handleSearch}>
+                <input
+                    className="bar"
+                    type="text"
+                    placeholder="Search for a topic"
+                    value={searchTerm}
+                    onChange={(e) => setSearchTerm(e.target.value)}
+                />
+                <button type="submit">Search</button>
+            </form>
+            {/* Display search results if available */}
+            {searchResults && (
+                <div className="search-results">
+                    {/* Render search results here */}
+                </div>
+            )}
         </div>
     );
 };
